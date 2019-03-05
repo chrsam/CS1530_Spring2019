9a4f9234abd9aa6d29d93c9ac545acb539bc479c
Peter Mulholland <pem42@pitt.edu>
2019-03-05 11:54:31 -0500

    Fix textboxes not updating

--- front-end/src/components/Register.js
+++ front-end/src/components/Register.js
@@ -4,38 +4,50 @@ import { Link } from 'react-router-dom';
 
 class Register extends Component {
 
-  state = {
-    firstName: "",
-    lastName: "",
-    email: "",
-    password: ""
-  };
 
-  submit(e) {
-    e.preventDefault();
+
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      firstName: "",
+      lastName: "",
+      email: "",
+      password: ""
+    };
+
+    this.handleChange = this.handleChange.bind(this)
+  }
+
+  submit(event) {
+    event.preventDefault(); 
     // REST calls go here
     console.log("Register button clicked");
   }
 
+  handleChange(event) {
+    this.setState({ [event.target.name] : event.target.value });  
+  }
+
   render() {
     return (
-      <div className="App">
+      <div>
         <Link to="/">Home</Link>
 
         <header className="App-header">
         <h1>Register</h1>
 
           <div>First name:</div>
-          <input type="text" name="email" value={this.state.firstName}></input>
+          <input type="text" name="firstName" value={this.state.firstName} onChange={this.handleChange}></input>
 
           <div>Last name:</div>
-          <input type="text" name="email" value={this.state.lastName}></input>
+          <input type="text" name="lastName" value={this.state.lastName} onChange={this.handleChange}></input>
 
           <div>Email address:</div>
-          <input type="text" name="email" value={this.state.email}></input>
+          <input type="text" name="email" value={this.state.email} onChange={this.handleChange}></input>
 
           <div>Password:</div>
-          <input type="text" name="password" value={this.state.password}></input>
+          <input type="text" name="password" value={this.state.password} onChange={this.handleChange}></input>
 
           <br/>
           <button type="button" onClick={this.submit}>Register</button>
