f406d39ae0ad31b71a017bfd681c98dd0e5ff7f2
aricke7 <aricke7@hotmail.com>
2019-04-10 11:57:58 -0400

    version2 i Please enter the commit message for your changes. Lines starting

--- ratemyclass/courses/models.py
+++
@@ -1,8 +0,0 @@
-from django.db import models
-from django.contrib.auth.models import User
-
-class Course(models.Model):
-    name = models.CharField(max_length=100)
-    class_code = models.CharField(max_length=10, unique=True)
-    # owner is the user who created this course
-    owner = models.ForeignKey(User, related_name="courses", on_delete=models.CASCADE, null=True)
--- ratemyclass/frontend/src/reducers/index.js
+++
@@ -1,8 +0,0 @@
-import { combineReducers } from 'redux';
-import courses from './courses';
-import auth from './auth';
-
-export default combineReducers({
-    courses,
-    auth
-});
